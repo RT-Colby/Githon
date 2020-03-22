1bad719e2dec2455bad7b78cae28c928b0407360
RT-Colby <cfrey@protonmail.com>
2020-03-21 19:06:05 -0700

    Clone repos

--- AccessMethods.py
+++ AccessMethods.py
@@ -10,51 +10,6 @@ import tkinter.ttk as tkrtk
 from tkinter import messagebox
 import platform
 
-def login(self,root,notebook,username,password,signInRoot):
-	self.root = root
-	self.repoNb = notebook
-	self.repoListBox =Listbox(self.root)
-	counter = 0
-	dic = {}
-	g = Github(username,password)
-	try:
-		g.get_user()
-	except:
-		messagebox.showerror('Required Field', 'Please enter a valid decryption key')
-		return
-	try:
-		self.homeSheet = tkrtk.Frame(self.repoNb)
-		dic['Home'] = self.homeSheet
-		for repo in g.get_user().get_repos():
-			self.tempSheet = tkrtk.Frame(self.repoNb)
-			dic[repo.name] = self.tempSheet
-			counter += 1
-		for frame in dic:
-			self.repoNb.add(dic[frame], text=frame)
-		self.repoNb.grid(row=0)
-		signInRoot.destroy()
-	except:
-		messagebox.showerror('Required Field', 'Invalid Login! \nPlease enter valid credentials')
-
-def readRepo(reponame,ghAcc,access_token = '',branch=''):
-	g = ghAcc
-	if platform.system == 'Linux':
-	    repoDir = os.path.expanduser('~/Repositores/')
-	    #Checks if the Repositories Directory exists
-	    if os.path.isdir(repoDir):
-	        for repo in g.get_user().get_repos():
-	            os.mkdir(path + repo.name)
-	            if repo.private:
-	                print('NI')
-	            else:
-	                Repo.clone_from(repo.clone_url, path + repo.name )        
-	    else:
-	        for repo in g.get_user().get_repos():
-	            os.makedirs(path + repo.name)
-	            Repo.clone_from(repo.clone_url, path + repo.name ) 
-	else:
-	    #User is on windows
-	    print('Not Implemented')
 
 def formatUrl(pubUrl,access_token):
     splitUrl = pubUrl.split('/')
@@ -62,5 +17,21 @@ def formatUrl(pubUrl,access_token):
     x = splitUrl[0] + r'//' + access_token + ':x-oauth-basic@github.com/' + splitUrl[3] + '/' + splitUrl[4]
     return x
 
+def readRepo(repo,ghAcc,access_token = '',branch=''):
+	g = ghAcc
+	if sys.platform == 'linux':
+	    repoDir = os.path.expanduser('~/Repositories/')
+	    #Checks if there is already a folder for the repo
+	    if os.path.isdir(repoDir + repo.name) != True:
+	    	#if the repo doesnt exsit, clone it 
+	    	if repo.private:
+	    		#Clones the repo using the access token
+	    		privUrl = formatUrl(repo.clone_url,access_token)
+	    		Repo.clone_from(privUrl, repoDir + repo.name)
+	    	else:
+	    		#Clones repo using http url
+	    		Repo.clone_from(repo.clone_url, repoDir + repo.name)
+	else:
+		print('Windows implemntation not done ')
 
 
--- Welcome_Page.py
+++ Welcome_Page.py
@@ -32,5 +32,29 @@ class Welcome_Page:
 		self.ghPasswordEntry.grid(row=2,column=1)
 		self.ghLoginButton.grid(row=3,column=1)
 
-
+def login(self,root,notebook,username,password,signInRoot):
+	self.root = root
+	self.repoNb = notebook
+	self.repoListBox =Listbox(self.root)
+	counter = 0
+	dic = {}
+	g = Github(username,password)
+	try:
+		g.get_user()
+	except:
+		messagebox.showerror('Required Field', 'Please enter a valid decryption key')
+		return
+	try:
+		self.homeSheet = tkrtk.Frame(self.repoNb)
+		dic['Home'] = self.homeSheet
+		for repo in g.get_user().get_repos():
+			self.tempSheet = tkrtk.Frame(self.repoNb)
+			dic[repo.name] = self.tempSheet
+			counter += 1
+		for frame in dic:
+			self.repoNb.add(dic[frame], text=frame)
+		self.repoNb.grid(row=0)
+		signInRoot.destroy()
+	except:
+		messagebox.showerror('Required Field', 'Invalid Login! \nPlease enter valid credentials')
 
